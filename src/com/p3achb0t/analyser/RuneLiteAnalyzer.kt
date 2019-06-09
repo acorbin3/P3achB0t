e04fc960a1eba295b770a6edf1840c0da532eb6d
Adam Corbin <acorbin3@gmail.com>
2019-06-09 05:18:11 -0400

    Generating code for RuneLite hooks interfaces

--- src/com/p3achb0t/analyser/RuneLiteAnalyzer.kt
+++ src/com/p3achb0t/analyser/RuneLiteAnalyzer.kt
@@ -8,7 +8,7 @@ import java.nio.channels.Channels
 
 class RuneLiteAnalyzer {
     private var webAddress: String =
-        "https://raw.githubusercontent.com/runelite-extended/runelite/master/extended-mixins/hooks.json"
+        "https://raw.githubusercontent.com/runelite-extended/runelite/eafb024f16821160aab966114962711d7a4d355e/extended-mixins/hooks.json"
     val hookDir = "\\hooks\\"
 
     var hookFile = ""
@@ -47,8 +47,58 @@ class RuneLiteAnalyzer {
 
     }
 
+    private fun isBaseType(discriptor: String): Boolean {
+        return when (discriptor) {
+            "C" -> true
+            "B" -> true
+            "S" -> true
+            "Z" -> true
+            "D" -> true
+            "J" -> true
+            "I" -> true
+            "F" -> true
+            "java/lang/String" -> true
+            else -> false
+        }
+    }
+
+    private fun getType(discriptor: String): String {
+        return when (discriptor) {
+            "C" -> "Char"
+            "B" -> "Byte"
+            "S" -> "Short"
+            "Z" -> "Boolean"
+            "D" -> "Double"
+            "J" -> "Long"
+            "I" -> "Int"
+            "F" -> "Float"
+            "java/lang/String" -> "String"
+            else -> discriptor
+        }
+    }
+
+    private fun getArrayString(count: Int, type: String): String {
+        return when (count) {
+            1 -> "Array<$type>"
+            2 -> "Array<Array<$type>>"
+            3 -> "Array<Array<Array<$type>>>"
+            4 -> "Array<Array<Array<Array<$type>>>>"
+            5 -> "Array<Array<Array<Array<Array<$type>>>>>"
+            else -> type
+        }
+    }
+
+    private fun isFieldNameUnique(clazz: RuneLiteJSONClasses.ClassDefinition, fieldName: String): Boolean {
+        return clazz.fields.count { it.field == fieldName } == 0
+    }
+
     fun createInterfaces() {
         val path = System.getProperty("user.dir")
+
+        // Delete files
+        val folder = File("C:\\Users\\C0rbin\\IdeaProjects\\P3achB0t\\src\\com\\p3achb0t\\_runelite_interfaces/")
+        folder.deleteRecursively()
+        folder.mkdir()
         for (clazz in analyzers) {
             val fn =
                 "C:\\Users\\C0rbin\\IdeaProjects\\P3achB0t\\src\\com\\p3achb0t\\_runelite_interfaces/" + clazz.value._class + ".kt"
@@ -63,46 +113,64 @@ class RuneLiteAnalyzer {
                 }
                 for (field in clazz.value.fields) {
 
-                    //x = fields["x"]?.resultValue?.toInt() ?: -1
-                    when {
-                        field.descriptor == "C" -> out.println("\t fun get_" + field.field + "(): Char")
-                        field.descriptor == "[C" -> out.println("\t fun get_" + field.field + "(): Array<Char>")
-                        field.descriptor == "B" -> out.println("\t fun get_" + field.field + "(): Byte")
-                        field.descriptor == "[B" -> out.println("\t fun get_" + field.field + "(): Array<Byte>")
-                        field.descriptor == "[[B" -> out.println("\t fun get_" + field.field + "(): Array<Array<Byte>>")
-                        field.descriptor == "[[[B" -> out.println("\t fun get_" + field.field + "(): Array<Array<Array<Byte>>>")
-                        field.descriptor == "S" -> out.println("\t fun get_" + field.field + "(): Short")
-                        field.descriptor == "[S" -> out.println("\t fun get_" + field.field + "(): Array<Short>")
-                        field.descriptor == "[[S" -> out.println("\t fun get_" + field.field + "(): Array<Array<Short>>")
-                        field.descriptor == "[[[S" -> out.println("\t fun get_" + field.field + "(): Array<Array<Array<Short>>>")
-                        field.descriptor == "Z" -> out.println("\t fun get_" + field.field + "(): Boolean")
-                        field.descriptor == "[Z" -> out.println("\t fun get_" + field.field + "(): Array<Boolean>")
-                        field.descriptor == "[[Z" -> out.println("\t fun get_" + field.field + "(): Array<Array<Boolean>>")
-                        field.descriptor == "[[[Z" -> out.println("\t fun get_" + field.field + "(): Array<Array<Array<Boolean>>>")
-                        field.descriptor == "D" -> out.println("\t fun get_" + field.field + "(): Double")
-                        field.descriptor == "J" -> out.println("\t fun get_" + field.field + "(): Long")
-                        field.descriptor == "[J" -> out.println("\t fun get_" + field.field + "(): Array<Long>")
-                        field.descriptor == "F" -> out.println("\t fun get_" + field.field + "(): Float")
-                        field.descriptor == "[F" -> out.println("\t fun get_" + field.field + "(): Array<Float>")
-                        field.descriptor == "[[F" -> out.println("\t fun get_" + field.field + "(): Array<Array<Float>>")
-                        field.descriptor == "I" -> out.println("\t fun get_" + field.field + "(): Int")
-                        field.descriptor == "[I" -> out.println("\t fun get_" + field.field + "(): Array<Int>")
-                        field.descriptor == "[[I" -> out.println("\t fun get_" + field.field + "(): Array<Array<Int>>")
-                        field.descriptor == "[[[I" -> out.println("\t fun get_" + field.field + "(): Array<Array<Array<Int>>>")
-                        field.descriptor == "Ljava/lang/String;" -> out.println("\t fun get_" + field.field + "(): String")
-                        field.descriptor == "[Ljava/lang/String;" -> out.println("\t fun get_" + field.field + "(): Array<String>")
-                        field.descriptor == "[[Ljava/lang/String;" -> out.println("\t fun get_" + field.field + "(): Array<Array<String>>")
-                        else -> {
-                            val arrayCount = field.descriptor.count { it == '[' }
-                            // Remove L and ; and find the interface for the class
-                            val trimmedDiscriptor = field.descriptor.substring(1, field.descriptor.length - 1)
-                            if (trimmedDiscriptor in classRefObs) {
-                                out.println("\t fun get_" + field.field + "(): " + classRefObs[trimmedDiscriptor]?._class)
-                            } else {
-                                println(field.descriptor)
-                                out.println("\t fun get_" + field.field + "(): Any")
-                            }
+                    val arrayCount = field.descriptor.count { it == '[' }
+                    var updatedDescriptor = field.descriptor
+                    //Trim array brackets
+                    if (arrayCount > 0) {
+                        updatedDescriptor = field.descriptor.substring(arrayCount, field.descriptor.length)
+//                        println("Removing array: ${field.descriptor}->$updatedDescriptor")
+                    }
+                    //Trim L; for long types
+                    if (field.descriptor.contains(";")) {
+                        updatedDescriptor = updatedDescriptor.substring(1, updatedDescriptor.length - 1)
+//                        println("Removing array: ${field.descriptor}->$updatedDescriptor")
 
+                    }
+//                    println("Array Count: $arrayCount $updatedDescriptor    ${field.descriptor}")
+
+                    if (updatedDescriptor in classRefObs) {
+                        out.println(
+                            "\t fun get_" + field.field + "(): ${getArrayString(
+                                arrayCount,
+                                getType(classRefObs[updatedDescriptor]?._class!!)
+                            )}"
+                        )
+                    } else {
+                        if (isBaseType(updatedDescriptor)) {
+                            //Check to ensure that there are no fields with the same name in the super class
+                            if (clazz.value._super in classRefObs) {
+//                                print("Checking ${field.field} in class ${clazz.value._class} file is in ${classRefObs[clazz.value._super]!!._class}")
+                                if (isFieldNameUnique(classRefObs[clazz.value._super]!!, field.field)) {
+                                    out.println(
+                                        "\t fun get_${field.field}(): ${getArrayString(
+                                            arrayCount,
+                                            getType(updatedDescriptor)
+                                        )}"
+                                    )
+                                } else {
+                                    println(
+                                        "^^^^^Not Unique \"\\t fun get_${clazz.value._class}_${field.field}(): ${getArrayString(
+                                            arrayCount,
+                                            getType(updatedDescriptor)
+                                        )}"
+                                    )
+                                    out.println(
+                                        "\t fun get_${clazz.value._class}_${field.field}(): ${getArrayString(
+                                            arrayCount,
+                                            getType(updatedDescriptor)
+                                        )}"
+                                    )
+                                }
+                            } else {
+                                out.println(
+                                    "\t fun get_${field.field}(): ${getArrayString(
+                                        arrayCount,
+                                        getType(updatedDescriptor)
+                                    )}"
+                                )
+                            }
+                        } else {
+                            out.println("\t fun get_" + field.field + "(): Any")
                         }
                     }
                 }
@@ -113,8 +181,8 @@ class RuneLiteAnalyzer {
 
     }
 
-    //TODO - create interface files
-    //TODO - make sure interface files are correctly superred
-    //TODO - Interface files have methods
+    //DONE - create interface files
+    //Done - make sure interface files are correctly superred
+    //DONE 06/09/2019 - Interface files have methods
     //TODO - Create code to inject
 }
