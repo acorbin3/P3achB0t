1f5966da553e8f78fc15340ea599b19f1892a95f
Adam Corbin <acorbin3@gmail.com>
2019-10-01 04:32:08 -0400

    removing old mouse code

--- src/com/p3achb0t/api/user_inputs/Mouse.kt
+++ src/com/p3achb0t/api/user_inputs/Mouse.kt
@@ -45,42 +45,9 @@ class Mouse(obj: Any) {
             return false
         }
 
-//        val startPoint = Point(ioMouse.getX(), ioMouse.getY())
-        print("Mouse Moving: $destPoint")
-        mouseMotionFactory?.move(destPoint.x, destPoint.y)
-//        val distance = startPoint?.distance(destPoint)
-//        val timeDurationMS = distance?.div(RATE_PIXELS_PER_SEC)?.times(1000).let { it?.let { it1 -> Math.floor(it1) } }
-//        val iterations = distance?.div(MIN_DIST_PIXELS)
-//        val interval = iterations?.let { timeDurationMS?.div(it) }
-//        val currentPoint = startPoint
-//        val deltaX = startPoint?.x?.let { iterations?.let { it1 -> destPoint.x.minus(it).div(it1) } }
-//        val deltaY = startPoint?.y?.let { iterations?.let { it1 -> destPoint.y.minus(it).div(it1) } }
-//        println("Distance: $distance timeinMS: $timeDurationMS iteration: $iterations interval: $interval deltaX:$deltaX deltaY: $deltaY")
-//        if (iterations != null) {
-//            val startX = startPoint.x
-//            val startY = startPoint.y
-//            for (i in 1..iterations.toInt() + 1) {
-//                val xOffset = deltaX?.times(i)?.let { Math.ceil(it) }
-//                val x = xOffset?.let { startX.plus(it) }
-//                val yOffset = deltaY?.times(i)?.let { Math.ceil(it) }
-//                val y = yOffset?.let { startY.plus(it) }
-//                val mouseMove = x?.toInt()?.let { _x ->
-//                    //                    println("$x $xOffset $startX    $y $yOffset $startY")
-//                    y?.toInt()?.let { _y ->
-//                        currentPoint?.x = _x
-//                        currentPoint?.y = _y
-//                        MouseEvent(
-//                            component,
-//                            MouseEvent.MOUSE_MOVED, System.currentTimeMillis(), 0, _x, _y, 0, false
-//                        )
-//                    }
-//                }
-//                ioMouse.sendEvent(mouseMove)
-//                interval?.toLong()?.let { delay(it) }
-//            }
-//        }
+        mouseMotionFactory.move(destPoint.x, destPoint.y)
+
         if (click) {
-            println("Pressing mouse down")
             delay(Random.nextLong(50, 150))
             val clickMask = if (clickType == ClickType.Right) MouseEvent.BUTTON3_MASK else MouseEvent.BUTTON1_MASK
             val mousePress =
@@ -100,7 +67,6 @@ class Mouse(obj: Any) {
             // Create a random number 30-70 to delay between clicks
             val delayTime = Math.floor(Math.random() * 40 + 30)
             delay(delayTime.toLong())
-            println("Releasing mouse")
             val mouseRelease =
                 MouseEvent(
                     component,
