d221a7d12ae73e3d09a6c0af70b7f985f6763ea0
blah <blah>
2020-01-08 08:22:13 +0000

    health/prayer check and str/current str level check
    # Conflicts:
    #	src/com/p3achb0t/api/wrappers/Player.kt

--- src/com/p3achb0t/api/wrappers/Player.kt
+++ src/com/p3achb0t/api/wrappers/Player.kt
@@ -1,8 +1,9 @@
 package com.p3achb0t.api.wrappers
 
 import com.p3achb0t.api.Calculations
-import com.p3achb0t.api.getConvexHull
 import com.p3achb0t.api.Context
+import com.p3achb0t.api.getConvexHull
+import com.p3achb0t.api.wrappers.widgets.WidgetItem
 import java.awt.Point
 import kotlin.math.abs
 import kotlin.math.max
@@ -14,7 +15,7 @@ class Player(var player: com.p3achb0t._runestar_interfaces.Player, ctx: Context)
         val players = ArrayList<Player>()
         ctx?.client?.getPlayers()?.forEach {
             if (it != null) {
-                players.add(Player(it,ctx ))
+                players.add(Player(it, ctx!!))
             }
         }
 
@@ -36,6 +37,34 @@ class Player(var player: com.p3achb0t._runestar_interfaces.Player, ctx: Context)
         return Calculations.worldToScreen(region.x, region.y, player.getHeight(), ctx!!)
     }
 
+    fun getHealth(): Int {
+        var healthInt = 0
+        val health = WidgetItem(ctx?.widgets?.find(160, 5), ctx = ctx).widget?.getText()
+        healthInt = health?.toInt()!!
+        return healthInt
+    }
+
+    fun getPrayer(): Int {
+        var healthInt = 0
+        val health = WidgetItem(ctx?.widgets?.find(160, 15), ctx = ctx).widget?.getText()
+        healthInt = health?.toInt()!!
+        return healthInt
+    }
+
+    fun getStrength(): Int {
+        var level = 0
+        var temp = WidgetItem(ctx?.widgets?.find(320, 2)?.getChildren()?.get(4), ctx = ctx)
+        level = temp.widget?.getText()?.toInt()!!
+        return level
+    }
+
+    fun getCurrentStrength(): Int {
+        var level = 0
+        var temp = WidgetItem(ctx?.widgets?.find(320, 2)?.getChildren()?.get(3), ctx = ctx)
+        level = temp.widget?.getText()?.toInt()!!
+        return level
+    }
+
     override suspend fun interact(action: String): Boolean {
         //TODO check is player is on screen
         //  TODO - Move camera for player to be on screen
@@ -51,7 +80,7 @@ class Player(var player: com.p3achb0t._runestar_interfaces.Player, ctx: Context)
                     this.player.getAppearance().getNpcTransformId().toLong(),
                     ctx!!)
             //Checking to see if this is on screen
-            Interact(ctx).interact(ch, action)
+            Interact(ctx!!).interact(ch, action)
         } catch (e: Exception) {
         }
         return false
