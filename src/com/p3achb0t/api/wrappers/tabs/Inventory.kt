defd5f1dcd490331f7ed718f0de2ae04de412353
blah <blah>
2020-01-08 08:21:34 +0000

    added logic for multiple potions in inventory
    # Conflicts:
    #	src/com/p3achb0t/api/wrappers/tabs/Inventory.kt

--- src/com/p3achb0t/api/wrappers/tabs/Inventory.kt
+++ src/com/p3achb0t/api/wrappers/tabs/Inventory.kt
@@ -1,10 +1,10 @@
 package com.p3achb0t.api.wrappers.tabs
 
 import com.p3achb0t._runestar_interfaces.Component
+import com.p3achb0t.api.Context
 import com.p3achb0t.api.wrappers.widgets.Widget
 import com.p3achb0t.api.wrappers.widgets.WidgetID
 import com.p3achb0t.api.wrappers.widgets.WidgetItem
-import com.p3achb0t.api.Context
 import java.awt.Rectangle
 
 class Inventory(val ctx: Context? = null) {
@@ -17,7 +17,6 @@ class Inventory(val ctx: Context? = null) {
     suspend fun open() {
 
         if (!isOpen()) Tabs(ctx!!).openTab(Tabs.Tab_Types.Inventory)
-        if (!isOpen()) open()
     }
 
     fun isOpen(): Boolean {
@@ -34,8 +33,8 @@ class Inventory(val ctx: Context? = null) {
 
             val columns = inventory.getWidth()
             val rows = inventory.getHeight()
-            val baseX = Widget.getWidgetX(inventory,ctx)
-            val baseY = Widget.getWidgetY(inventory,ctx )
+            val baseX = Widget.getWidgetX(inventory, ctx)
+            val baseY = Widget.getWidgetY(inventory, ctx)
             for (i in 0 until (columns * rows)) {
                 if (ids[i] > 0 && stacks[i] > 0) {
                     val row = i / columns
@@ -59,13 +58,115 @@ class Inventory(val ctx: Context? = null) {
         return items
     }
 
-    fun getItem(id: Int): WidgetItem? {
-        println("Looking for Item $id")
-        val items = getAll()
-        items.forEach {
-            if (it.id == id) {
-                println("Found Item! $id")
-                return it
+    fun hasPrayerPots(): Boolean {
+        var HasItems = false
+        val ItemsNeeded: IntArray = intArrayOf(2434, 139, 141, 143)
+        ItemsNeeded.forEach {
+            if (getCount(it) > 0) {
+                HasItems = true
+            }
+        }
+        return HasItems
+    }
+
+
+    fun getPrayerDoses(): Int {
+        var doses = 0
+        val ItemsNeeded: IntArray = intArrayOf(2434, 139, 141, 143)
+        ItemsNeeded.forEach {
+            if (it == 2434) {
+                if (getCount(it) > 0) {
+                    doses = doses + (getCount(it) * 4)
+                }
+            }
+            if (it == 143) {
+                if (getCount(it) > 0) {
+                    doses = doses + (getCount(it) * 3)
+                }
+            }
+            if (it == 141) {
+                if (getCount(it) > 0) {
+                    doses = doses + (getCount(it) * 2)
+                }
+            }
+            if (it == 139) {
+                if (getCount(it) > 0) {
+                    doses = doses + getCount(it)
+                }
+            }
+
+        }
+        return doses
+    }
+
+    fun hasextendedSuperAntiFire(): Boolean {
+        var HasItems = false
+        val ItemsNeeded: IntArray = intArrayOf(22209, 22212, 22215, 22218)
+        ItemsNeeded.forEach {
+            if (getCount(it) > 0) {
+                HasItems = true
+            }
+        }
+        return HasItems
+    }
+
+    fun hasextendedAntiFire(): Boolean {
+        var HasItems = false
+        val ItemsNeeded: IntArray = intArrayOf(11951, 11953, 11955, 11957)
+        ItemsNeeded.forEach {
+            if (getCount(it) > 0) {
+                HasItems = true
+            }
+        }
+        return HasItems
+    }
+
+    fun hasDivineCombats(): Boolean {
+        var HasItems = false
+        val ItemsNeeded: IntArray = intArrayOf(23685, 23688, 23691, 23694)
+        ItemsNeeded.forEach {
+            if (getCount(it) > 0) {
+                HasItems = true
+            }
+        }
+        return HasItems
+    }
+
+    fun hasDueling(): Boolean {
+        var HasItems = false
+        val ItemsNeeded: IntArray = intArrayOf(2552, 2554, 2556, 2558, 2560, 2562, 2564, 2566)
+        ItemsNeeded.forEach {
+            if (getCount(it) > 0) {
+                HasItems = true
+            }
+        }
+        return HasItems
+    }
+
+    fun hasPendant(): Boolean {
+        var HasItems = false
+        val ItemsNeeded: IntArray = intArrayOf(11194, 11193, 11192, 11191, 11190)
+        ItemsNeeded.forEach {
+            if (getCount(it) > 0) {
+                HasItems = true
+            }
+        }
+        return HasItems
+    }
+
+    suspend fun getItem(id: Int): WidgetItem? {
+        if (!isOpen()) {
+            open()
+        }
+        if (isOpen()) {
+
+            println("Looking for Item $id")
+            val items = getAll()
+            items.forEach {
+                if (it.id == id) {
+                    println("Found Item! $id")
+                    return it
+                }
             }
         }
         return null
@@ -99,12 +200,25 @@ class Inventory(val ctx: Context? = null) {
         return count
     }
 
+
     fun getCount(itemID: Int, useStack: Boolean = false): Int {
         var count = 0
         for (widget in getAll()) {
             if (widget.id == itemID) count += if (useStack) widget.stackSize else 1
         }
         return count
+    }
+
+    /**
+     * added by sirscript
+     */
+
+    fun Contains(id: Int): Boolean {
+        var Contains = false
+        for (widget in getAll()) {
+            if (widget.id == id) Contains = true
+        }
+        return Contains
     }
 
 }
