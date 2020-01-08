8fc7d810387e5bdd635704528627afe48831be46
blah <blah>
2020-01-08 09:08:44 +0000

    Fixed client logging in for when account disconnects

--- src/com/p3achb0t/api/LoggingIntoClient.kt
+++ src/com/p3achb0t/api/LoggingIntoClient.kt
@@ -23,15 +23,20 @@ fun LoggingIntoAccount(ctx: Context) {
         repeat(1000) {
             try {
 
+//                val disconnected = WidgetItem(ctx.widgets.find(WidgetID.login, 85), ctx = ctx)
                 // When loaded login
-                if (!LoggingIntoClient.loggedIn && ctx.client.getGameState() == 10) {
+                if (ctx.client.getGameState() == 10) {
+                    delay(2000)
+                    ctx.mouse.moveMouse(Point(399, 316), true, Mouse.ClickType.Left)
+                    delay(1000)
                     ctx.mouse.moveMouse(Point(531, 280), true, Mouse.ClickType.Left)
-
+                    delay(2000)
+                    ctx.keyboard.sendKeys(UserDetails.data.username)
+                    delay(500)
+                    ctx.mouse.moveMouse(Point(367, 262), true, Mouse.ClickType.Left)
                     delay(2000)
                     ctx.keyboard.sendKeys(UserDetails.data.password)
-                    delay(500)
-
-                    ctx.mouse.moveMouse(Point(300, 310), true, Mouse.ClickType.Left)
+                    ctx.mouse.moveMouse(Point(326, 323), true, Mouse.ClickType.Left)
 
                     while (ctx.client.getGameState() != 30) {
                         delay(100)
@@ -40,9 +45,13 @@ fun LoggingIntoAccount(ctx: Context) {
                     println("Clicking login")
                     val login = WidgetItem(ctx.widgets.find(WidgetID.LOGIN_CLICK_TO_PLAY_GROUP_ID, 85), ctx = ctx)
                     println("login: ${login.area.x},${login.area.y},${login.area.height},${login.area.width}")
-
                     login.click()
-                    LoggingIntoClient.loggedIn = true
+                    Utils.waitFor(5, object : Utils.Condition {
+                        override suspend fun accept(): Boolean {
+                            delay(100)
+                            return ctx.client.getGameState() == 30
+                        }
+                    })
                 }
 
 
