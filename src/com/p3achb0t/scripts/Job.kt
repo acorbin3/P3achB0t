2dd42ec080ebaba60daee75b1effd22319cd0ed6
blah <blah>
2020-01-08 09:05:31 +0000

    added task

--- src/com/p3achb0t/scripts/Job.kt
+++ src/com/p3achb0t/scripts/Job.kt
@@ -3,7 +3,13 @@ package com.p3achb0t.scripts
 import com.p3achb0t._runestar_interfaces.Client
 import com.p3achb0t.api.wrappers.widgets.WidgetItem
 
+
 abstract class Job(val client: Client) {
     abstract suspend fun isValidToRun(dialogWidget: WidgetItem): Boolean
+    abstract suspend fun execute()
+}
+
+abstract class Task(val client: Client) {
+    abstract suspend fun isValidToRun(): Boolean
     abstract suspend fun execute()
 }
