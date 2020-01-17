1a4df500a6c3cee38f4d4db88f1f1d92e8a3c101
Adam Corbin <acorbin3@gmail.com>
2020-01-17 06:44:56 -0500

    Adding doAction callback

--- src/com/p3achb0t/analyser/Analyzer.kt
+++ src/com/p3achb0t/analyser/Analyzer.kt
@@ -208,21 +208,24 @@ class Analyser{
                 println("looking at class ${methodHook.owner}")
                 classes[methodHook.owner]?.methods?.forEach { methodNode ->
                     println("Looking at method: ${methodNode.name}")
-                    if (methodNode.name == methodHook.name && methodNode.desc.contains("IIIILjava/lang/String;Ljava/lang/String;III")) {
+                    if (methodNode.name == methodHook.name ) {
                         println("methodNode.desc: ${methodNode.desc}")
                         println("Time to insert instructions")
                         val il = InsnList()
                         il.add(FieldInsnNode(GETSTATIC, "client", "script", "Lcom/p3achb0t/interfaces/ScriptManager;"))
+                        il.add(VarInsnNode(ILOAD, 0))
                         il.add(VarInsnNode(ILOAD, 1))
                         il.add(VarInsnNode(ILOAD, 2))
                         il.add(VarInsnNode(ILOAD, 3))
-                        il.add(VarInsnNode(ILOAD, 4))
+                        il.add(VarInsnNode(ALOAD, 4))
                         il.add(VarInsnNode(ALOAD, 5))
-                        il.add(VarInsnNode(ALOAD, 6))
+                        il.add(VarInsnNode(ILOAD, 6))
                         il.add(VarInsnNode(ILOAD, 7))
                         il.add(VarInsnNode(ILOAD, 8))
-                        il.add(VarInsnNode(ILOAD, 9))
-                        il.add(MethodInsnNode(INVOKEVIRTUAL, "com/p3achb0t/interfaces/ScriptManager", "doActionCallback", "(IIIILjava/lang/String;Ljava/lang/String;III)V"))
+                        il.add(MethodInsnNode(INVOKEVIRTUAL, "com/p3achb0t/interfaces/ScriptManager", "doActionCallback", methodHook.descriptor))
+//                        il.add(FieldInsnNode(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"))
+//                        il.add(LdcInsnNode("."));
+//                        il.add(MethodInsnNode(INVOKEVIRTUAL, "java/io/PrintStream", "print", "(Ljava/lang/String;)V"))
                         methodNode.instructions.insert(il)
 
                     }
