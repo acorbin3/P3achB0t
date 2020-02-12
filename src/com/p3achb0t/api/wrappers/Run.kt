41311eeac0bab0e6abc0ff408c7519719a54a9c8
Adam Corbin <acorbin3@gmail.com>
2020-02-12 05:16:52 -0500

    Adding doAction for run

--- src/com/p3achb0t/api/wrappers/Run.kt
+++ src/com/p3achb0t/api/wrappers/Run.kt
@@ -1,7 +1,9 @@
 package com.p3achb0t.api.wrappers
 
 import com.p3achb0t.api.Context
+import com.p3achb0t.api.user_inputs.DoActionParams
 import com.p3achb0t.api.wrappers.widgets.WidgetItem
+import net.runelite.api.MenuOpcode
 
 class Run(val ctx: Context) {
     companion object {
@@ -23,6 +25,15 @@ class Run(val ctx: Context) {
     suspend fun activateRun() {
         if (!isRunActivated()) {
             clickRunButton()
+        }
+    }
+
+    suspend fun activateRunDoAction(){
+        if(!isRunActivated()){
+            //argument0:-1, argument1:10485782, argument2:57, argument3:1, action:Toggle Run, targetName:, mouseX:641, mouseY:130, argument8:-1223904486
+            val doActionParams = DoActionParams(-1, 10485782, MenuOpcode.WIDGET_DEFAULT.id, 1, "", "", 0 ,0)
+            ctx.mouse.overrideDoActionParams = true
+            ctx.mouse.doAction(doActionParams)
         }
     }
 
