bb1118485742c881b85cf35c6b3e23276952545d
Adam Corbin <acorbin3@gmail.com>
2020-02-16 02:45:46 -0500

    #54 - Adding some try  catch blocks around the vars getter functions since they can throw some errors sometimes.

--- src/com/p3achb0t/api/wrappers/Vars.kt
+++ src/com/p3achb0t/api/wrappers/Vars.kt
@@ -7,8 +7,45 @@ import com.p3achb0t.api.Context
     like birdhouses, rune pouches, minigames, and even quest progress
  */
 class Vars(val ctx: Context) {
-    fun getVarbit(id: Int): Int  {return ctx.client.getVarbit(id)}
-    fun getVarp(id: Int): Int  { return ctx.client.getVarps_main()[id] }
-//    fun getVarcInt(id: Int): Int  { return ctx.client.getVarcs().get }
-    fun getVarcString(id: Int): String? { return ctx.client.getVarcs().getStrings()[id]}
+    /**
+     * This method will get the varbit within the client.
+     * @exception:There could be a time that this throws an exception. Return back a -1 in that case.
+     */
+    fun getVarbit(id: Int): Int {
+        return try {
+            ctx.client.getVarbit(id)
+        } catch (e: Exception) {
+            println("Varbit threw an exception")
+            e.stackTrace.iterator().forEach {
+                println(it)
+            }
+            -1
+        }
+
+    }
+
+    fun getVarp(id: Int): Int {
+        return try {
+            ctx.client.getVarps_main()[id]
+        } catch (e: Exception) {
+            println("getVarp threw an exception")
+            e.stackTrace.iterator().forEach {
+                println(it)
+            }
+            -1
+        }
+    }
+
+    //    fun getVarcInt(id: Int): Int  { return ctx.client.getVarcs().get }
+    fun getVarcString(id: Int): String? {
+        return try {
+            ctx.client.getVarcs().getStrings()[id]
+        } catch (e: Exception) {
+            println("getVarcString threw an exception")
+            e.stackTrace.iterator().forEach {
+                println(it)
+            }
+            ""
+        }
+    }
 }
