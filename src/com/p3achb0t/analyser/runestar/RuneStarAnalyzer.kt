64db8903f5ccf6ec3a28338c915707ac02947b8e
Adam Corbin <acorbin3@gmail.com>
2020-03-29 04:14:47 -0400

    #52 correctly using y and z now

--- src/com/p3achb0t/analyser/runestar/RuneStarAnalyzer.kt
+++ src/com/p3achb0t/analyser/runestar/RuneStarAnalyzer.kt
@@ -28,33 +28,6 @@ class RuneStarAnalyzer {
         }
 //        val json = file.readText() // your json value here
         val topic = Gson().fromJson(json, Array<ClassHook>::class.java)
-        //RuneStar has the Y and Z flipped from how our client works. Need to update the field name
-        val yTemp = topic.filter { it.`class` == "Client" }[0].fields.filter { it.field =="cameraY" }[0].copy()
-        val zTemp = topic.filter { it.`class` == "Client" }[0].fields.filter { it.field =="cameraZ" }[0].copy()
-
-        //Update by swapping names of the field
-        topic.iterator().forEach {
-            if(it.`class` == "Client"){
-                it.fields.iterator().forEach { fieldHook ->
-                    if(fieldHook.field == "cameraY"){
-                        println("Updating CameraY from ${fieldHook.name}->$zTemp")
-                        fieldHook.name = zTemp.name
-                        fieldHook.owner = zTemp.owner
-                        fieldHook.decoder = zTemp.decoder
-
-                    }
-                    if(fieldHook.field == "cameraZ"){
-                        println("Updating CameraZ from ${fieldHook.name}->${yTemp}")
-                        fieldHook.name = yTemp.name
-                        fieldHook.owner = yTemp.owner
-                        fieldHook.decoder = yTemp.decoder
-                    }
-                }
-            }
-        }
-
-        println("cameraZ = ${topic.filter { it.`class` == "Client" }[0].fields.filter { it.field =="cameraZ" }[0]}")
-        println("cameraY = ${topic.filter { it.`class` == "Client" }[0].fields.filter { it.field =="cameraY" }[0]}")
 
         for (clazz in topic) {
 
