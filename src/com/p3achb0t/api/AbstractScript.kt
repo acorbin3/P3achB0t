45156e614a3eed4edbeaf5cae01bee0fc46ee2c2
Kasper Løhde <kasperloehde@gmail.com>
2020-04-01 23:24:06 +0200

    Broke AbstractScript loading - works kinda

--- src/com/p3achb0t/api/AbstractScript.kt
+++ src/com/p3achb0t/api/AbstractScript.kt
@@ -3,7 +3,6 @@ package com.p3achb0t.api
 import java.awt.Graphics
 
 abstract class AbstractScript: SuperScript() {
-
     abstract suspend fun loop()
     abstract fun start()
     abstract fun stop()
