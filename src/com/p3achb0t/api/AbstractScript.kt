0dd3e0b51234720fa3fbefbad6e77e2939719713
Kasper Løhde <kasperloehde@gmail.com>
2020-04-01 20:06:02 +0200

    FIXED THE KEYBOARD

--- src/com/p3achb0t/api/AbstractScript.kt
+++ src/com/p3achb0t/api/AbstractScript.kt
@@ -7,7 +7,7 @@ abstract class AbstractScript: SuperScript() {
     var validate: Boolean = false
 
     abstract suspend fun loop()
-    abstract suspend fun start()
+    abstract fun start()
     abstract fun stop()
     open fun draw(g: Graphics) {}
 }
