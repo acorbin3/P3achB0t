0ba1c2e11f463f221e1d578658a7553f930820e4
Kasper Løhde <kasperloehde@gmail.com>
2020-04-01 20:07:19 +0200

    Added super

--- src/com/p3achb0t/api/AbstractScript.kt
+++ src/com/p3achb0t/api/AbstractScript.kt
@@ -4,8 +4,6 @@ import java.awt.Graphics
 
 abstract class AbstractScript: SuperScript() {
 
-    var validate: Boolean = false
-
     abstract suspend fun loop()
     abstract fun start()
     abstract fun stop()
