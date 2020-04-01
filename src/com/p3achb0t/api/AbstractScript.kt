7f39b94205f72db19b800c6ce8fefc5f5067f085
Sezonis <memes@memes.com>
2020-04-01 21:48:05 +1300

    added getters for main context methods into abstract script

--- src/com/p3achb0t/api/AbstractScript.kt
+++ src/com/p3achb0t/api/AbstractScript.kt
@@ -1,6 +1,13 @@
 package com.p3achb0t.api
 
+import com.p3achb0t.api.userinputs.Camera
+import com.p3achb0t.api.userinputs.Keyboard
+import com.p3achb0t.api.userinputs.Mouse
 import com.p3achb0t.api.utils.Logging
+import com.p3achb0t.api.wrappers.*
+import com.p3achb0t.api.wrappers.quests.QuestData
+import com.p3achb0t.api.wrappers.tabs.*
+import com.p3achb0t.api.wrappers.widgets.Widgets
 import java.awt.Graphics
 
 abstract class AbstractScript: Logging() {
@@ -12,4 +19,156 @@ abstract class AbstractScript: Logging() {
     abstract suspend fun start()
     abstract fun stop()
     open fun draw(g: Graphics) {}
+
+    val localPlayer : Player
+        get() {
+            return ctx.players.getLocal()
+        }
+
+    val players : Players
+        get()  {
+            return ctx.players
+        }
+
+
+    val groundItems : GroundItems
+        get()  {
+            return ctx.groundItems
+        }
+
+    val npcs : NPCs
+        get()  {
+            return ctx.npcs
+        }
+
+    val mouse : Mouse
+        get()  {
+            return ctx.mouse
+        }
+
+    val keyboard : Keyboard
+        get()  {
+            return ctx.keyboard
+        }
+
+    val camera : Camera
+        get()  {
+            return ctx.camera
+        }
+
+    val bank : Bank
+        get()  {
+            return ctx.bank
+        }
+
+    val clientMode : ClientMode
+        get()  {
+            return ctx.clientMode
+        }
+
+    val dialog : Dialog
+        get()  {
+            return ctx.dialog
+        }
+
+    val gameObjects : GameObjects
+        get()  {
+            return ctx.gameObjects
+        }
+
+    val items : Items
+        get()  {
+            return ctx.items
+        }
+
+    val menu : Menu
+        get()  {
+            return ctx.menu
+        }
+
+    val miniMap : MiniMap
+        get()  {
+            return ctx.miniMap
+        }
+
+    val equipment : Equipment
+        get()  {
+            return ctx.equipment
+        }
+
+    val inventory : Inventory
+        get()  {
+            return ctx.inventory
+        }
+
+    val magic : Magic
+        get()  {
+            return ctx.magic
+        }
+
+    val prayer : Prayer
+        get()  {
+            return ctx.prayer
+        }
+
+    val tabs : Tabs
+        get()  {
+            return ctx.tabs
+        }
+
+    val widgets : Widgets
+        get()  {
+            return ctx.widgets
+        }
+
+    val vars : Vars
+        get()  {
+            return ctx.vars
+        }
+
+    val cache : Cache
+        get()  {
+            return ctx.cache
+        }
+
+    val run : Run
+        get()  {
+            return ctx.run
+        }
+
+    val stats : Stats
+        get()  {
+            return ctx.stats
+        }
+
+    val questData : QuestData
+        get()  {
+            return ctx.questData
+        }
+
+    val projectiles : Projectiles
+        get()  {
+            return ctx.projectiles
+        }
+
+    val worldHop : WorldHop
+        get()  {
+            return ctx.worldHop
+        }
+
+    val grandExchange : GrandExchange
+        get()  {
+            return ctx.grandExchange
+        }
+
+    val trade : Trade
+        get()  {
+            return ctx.trade
+        }
+
+    val ipc : Channels
+        get()  {
+            return ctx.ipc
+        }
+
 }
