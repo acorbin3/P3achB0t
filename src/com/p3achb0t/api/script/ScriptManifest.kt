7b6ffe881462601ebb98472ad0bd96ad0b612450
Adam Corbin <acorbin3@gmail.com>
2020-04-20 06:09:57 -0400

    Upgrading to new api format

--- src/com/p3achb0t/api/script/ScriptManifest.kt
+++ src/com/p3achb0t/api/script/ScriptManifest.kt
@@ -1,4 +1,4 @@
 package com.p3achb0t.api.script
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ScriptManifest(val category: String, val name: String, val author: String, val version: String = "1.0")
+annotation class ScriptManifest(val category: String, val name: String, val author: String, val version: String)
