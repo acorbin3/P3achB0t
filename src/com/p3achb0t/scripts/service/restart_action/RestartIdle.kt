58fb621395fc6bbd15656a19afe00a85b1808a51
Adam Corbin <acorbin3@gmail.com>
2020-10-02 15:41:26 -0400

    Adding Restart Idle service script. This allows restarting the script when sitting idle for 10 min.

---
+++ src/com/p3achb0t/scripts/service/restart_action/RestartIdle.kt
@@ -0,0 +1,66 @@
+package com.p3achb0t.scripts.service.restart_action
+
+import com.p3achb0t.api.StopWatch
+import com.p3achb0t.api.script.ScriptManifest
+import com.p3achb0t.api.script.ServiceScript
+import com.p3achb0t.api.utils.Script
+import com.p3achb0t.api.wrappers.Tile
+import com.p3achb0t.client.accounts.Account
+import com.p3achb0t.scripts.paint.debug.DebugText
+import java.awt.Color
+import java.awt.Font
+import java.awt.Graphics
+
+@ScriptManifest(Script.SERVICE, "RestartIdle", "P3aches", "0.1")
+class RestartIdle : ServiceScript() {
+    var initilized = false
+    val idleStopWatch = StopWatch()
+    val currentTile = Tile()
+    var shouldRestart = false
+    val idleThresholdInMin = 10
+
+    override suspend fun loop(account: Account) {
+        println("Running restart idel")
+        if(!ctx.worldHop.isLoggedIn
+                || ctx.worldHop.isWelcomeRedButtonAvailable()
+                || (ctx.worldHop.isLoggedIn
+                        && (currentTile.x != ctx.players.getLocal().getGlobalLocation().x
+                        || currentTile.y != ctx.players.getLocal().getGlobalLocation().y))
+        ){
+            idleStopWatch.reset()
+            if(ctx.worldHop.isLoggedIn) {
+                currentTile.x = ctx.players.getLocal().getGlobalLocation().x
+                currentTile.y = ctx.players.getLocal().getGlobalLocation().y
+            }
+        }
+
+        if(ctx.worldHop.isLoggedIn
+                && idleStopWatch.elapsedMin >= idleThresholdInMin){
+            shouldRestart = true
+        }
+
+    }
+
+    override fun draw(g: Graphics) {
+
+        val debugText = arrayListOf<DebugText>()
+        debugText.add(DebugText("Idle time: ${idleStopWatch.getRuntimeString()}"))
+        if(ctx.worldHop.isLoggedIn) {
+            debugText.add(DebugText("saved tile: $currentTile "))
+            debugText.add(DebugText("Player tile: ${ctx.players.getLocal().getGlobalLocation()}"))
+        }else{
+            idleStopWatch.reset()
+        }
+        val x = 10
+        var y = 150
+        g.font = Font("Consolas", Font.PLAIN, 12)
+        debugText.forEach {
+            g.color = it.color
+            g.color = Color.green
+            g.drawString(it.text, x, y)
+            y += 13
+        }
+
+        super.draw(g)
+    }
+}
