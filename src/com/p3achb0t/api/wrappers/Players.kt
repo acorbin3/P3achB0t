6918d6ee5ae67b11d14a9d76f609348e38e53e99
Adam Corbin <acorbin3@gmail.com>
2020-12-24 13:11:00 -0500

    Updating API to be able to pass in a list of Players

--- src/com/p3achb0t/api/wrappers/Players.kt
+++ src/com/p3achb0t/api/wrappers/Players.kt
@@ -5,6 +5,9 @@ import kotlin.math.abs
 import kotlin.math.max
 
 class Players(val ctx: Context) {
+
+
+
     fun getLocal(): Player {
         return Player(ctx.client.getLocalPlayer(), ctx, 0)
     }
@@ -16,35 +19,38 @@ class Players(val ctx: Context) {
     fun getBaseY(): Int {
         return ctx.client.getLocalPlayer().getY() / 128 + ctx.client.getBaseY()
     }
-       // This function will return a list of NPCs with closes distance to you
-    fun findPlayers(playerName: String, sortByDist: Boolean = false): ArrayList<Player> {
-           val players = ArrayList<Player>()
-           return try {
-               ctx.client.getPlayers().forEachIndexed { index, player ->
-                   if(player != null){
 
-                   }
-                   if (player != null && player.getUsername().toString().contains(playerName, true)) {
-                       players.add(Player(player, ctx, index))
-                   }
-                   if (player != null && player.getUsername().getCleanName().replace("_", " ").toString().contains(playerName, true)) {
-                       players.add(Player(player, ctx, index))
-                   }
-               }
-               if (sortByDist) {
-                   players.sortBy {
-                       // Sort closest to player
-                       val localPlayer = ctx.client.getLocalPlayer()
-                       max(
-                               abs(localPlayer.getX() - it.player.getX()),
-                               abs(localPlayer.getY() - it.player.getY())
-                       )
-                   }
-               }
-               return players
-           } catch (e: Exception) {
-               return players
-           }
+    fun findPlayers(listOfPlayers: ArrayList<String>, sortByDist: Boolean = false): ArrayList<Player>{
+        val lowerCaseListOfPlayers = listOfPlayers.map { it.toLowerCase() }
+        val players = ArrayList<Player>()
+        try {
+            ctx.client.getPlayers().forEachIndexed { index, player ->
+                if (player != null && player.getUsername().toString().toLowerCase() in lowerCaseListOfPlayers) {
+                    players.add(Player(player, ctx, index))
+                }
+                if (player != null && player.getUsername().getCleanName().replace("_", " ").toLowerCase() in lowerCaseListOfPlayers) {
+                    players.add(Player(player, ctx, index))
+                }
+            }
+            if (sortByDist) {
+                players.sortBy {
+                    // Sort closest to player
+                    val localPlayer = ctx.client.getLocalPlayer()
+                    max(
+                            abs(localPlayer.getX() - it.player.getX()),
+                            abs(localPlayer.getY() - it.player.getY())
+                    )
+                }
+            }
+            return players
+        } catch (e: Exception) {
+            return players
+        }
+    }
+
+       // This function will return a list of players with closes distance to you
+    fun findPlayers(playerName: String, sortByDist: Boolean = false): ArrayList<Player> {
+           return findPlayers(arrayListOf(playerName), sortByDist)
     }
 
     // This function will return a list of NPCs with closes distance to you
