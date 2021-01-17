6d2aafaca42e83f0345ec7e783d7c63015342ca6
Adam Corbin <acorbin3@gmail.com>
2021-01-17 05:42:51 -0500

    Updating leaf task to have jobs/threads so they can be killed off when a script is paused.

--- src/com/p3achb0t/api/script/LeafTask.kt
+++ src/com/p3achb0t/api/script/LeafTask.kt
@@ -2,12 +2,13 @@ package com.p3achb0t.api.script
 
 import com.p3achb0t.api.Context
 import com.p3achb0t.api.utils.Logging
+import kotlinx.coroutines.Job
 
 /*This class is responsible for have a condition to be evaluated and upon true the base set of code
 * in execute would be run
 *
 * canBeSuspended: This is intended to allow */
-abstract class LeafTask(val ctx: Context, val canBeSuspended: Boolean = true): Logging() {
+abstract class LeafTask(val ctx: Context, val canBeSuspended: Boolean = true, val jobs: ArrayList<Job> = arrayListOf()): Logging() {
 
     abstract suspend fun isValidToRun(): Boolean
     abstract suspend fun execute()
