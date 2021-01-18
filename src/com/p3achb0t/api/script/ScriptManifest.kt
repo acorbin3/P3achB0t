54e07ae5df448d88b69c9cc8cebf0ea15e0075a3
Adam Corbin <acorbin3@gmail.com>
2021-01-18 02:54:45 -0500

    Updating script manafest to help filter. might be useful in the future

--- src/com/p3achb0t/api/script/ScriptManifest.kt
+++ src/com/p3achb0t/api/script/ScriptManifest.kt
@@ -1,4 +1,4 @@
 package com.p3achb0t.api.script
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ScriptManifest(val category: String, val name: String, val author: String, val version: String)
+annotation class ScriptManifest(val category: String, val name: String, val author: String, val version: String, val filter: Boolean = false)
