7ae61fc8c82170603f0dd9776dfc1d707460861e
Adam Corbin <acorbin3@gmail.com>
2021-01-31 02:26:59 -0500

    Attempting to get npc names better

--- src/com/p3achb0t/api/wrappers/NPC.kt
+++ src/com/p3achb0t/api/wrappers/NPC.kt
@@ -1,6 +1,8 @@
 package com.p3achb0t.api.wrappers
 
 import com.p3achb0t.api.Context
+import com.p3achb0t.api.interfaces.LocType
+import com.p3achb0t.api.interfaces.NPCType
 import com.p3achb0t.api.interfaces.Npc
 import com.p3achb0t.api.wrappers.utils.Calculations
 import com.p3achb0t.api.wrappers.utils.getConvexHull
@@ -12,7 +14,8 @@ class NPC(var npc: Npc, ctx: Context, val menuIndex: Int) : Actor(npc, ctx) {
 
     val name: String
         get() {
-            return npc.getType().getName()
+            val objectComposite = getType(npc.getType().getId())
+            return objectComposite?.getName().toString()
         }
 
     override fun isMouseOverObj(): Boolean {
@@ -75,14 +78,17 @@ class NPC(var npc: Npc, ctx: Context, val menuIndex: Int) : Actor(npc, ctx) {
         val x = getGlobalLocation().x
         val y = getGlobalLocation().y
 
+
+        //NorthEast
+        generatedTiles.add(Tile(x + maxOffset+1, y - maxOffset, ctx = ctx))
+
         //NorthWest
         generatedTiles.add(Tile(x - maxOffset, y - maxOffset, ctx = ctx))
-        //NorthEast
-        generatedTiles.add(Tile(x + maxOffset, y - maxOffset, ctx = ctx))
+
         //SouthWest
-        generatedTiles.add(Tile(x - maxOffset, y + maxOffset, ctx = ctx))
+        generatedTiles.add(Tile(x - maxOffset, y + maxOffset+1, ctx = ctx))
         //SouthEast
-        generatedTiles.add(Tile(x + maxOffset, y + maxOffset, ctx = ctx))
+        generatedTiles.add(Tile(x + maxOffset+1, y + maxOffset+1, ctx = ctx))
 
         return Area(generatedTiles, ctx)
     }
@@ -96,12 +102,52 @@ class NPC(var npc: Npc, ctx: Context, val menuIndex: Int) : Actor(npc, ctx) {
         //Sides
         for (offset in -maxOffset..maxOffset) {
             //sides
-            generatedTiles.add(Tile(x - maxOffset, y + offset, ctx = ctx))
-            generatedTiles.add(Tile(x + maxOffset, y + offset, ctx = ctx))
-            //top and bottom
-            generatedTiles.add(Tile(x + offset, y - maxOffset, ctx = ctx))
-            generatedTiles.add(Tile(x + maxOffset, y + maxOffset, ctx = ctx))
+            generatedTiles.add(Tile(x - maxOffset, y + offset, z=ctx?.client?.getPlane()?:0, ctx = ctx))
+            generatedTiles.add(Tile(x + maxOffset, y + offset, z=ctx?.client?.getPlane()?:0, ctx = ctx))
+            //Left and right side
+            generatedTiles.add(Tile(x + offset, y - maxOffset, z=ctx?.client?.getPlane()?:0, ctx = ctx))
+            generatedTiles.add(Tile(x + offset, y + maxOffset, z=ctx?.client?.getPlane()?:0, ctx = ctx))
         }
         return generatedTiles
+    }
+
+    val type = getType(npc.getType().getId())
+
+    fun getType(id: Int): NPCType? {
+        this.ctx!!.client.getNPCType_cached().getHashTable().getBuckets().iterator().forEach { bucketItem ->
+            if (bucketItem != null) {
+                var type = bucketItem.getNext()
+                while (type != null && type is NPCType) {
+                    if (type.getKey().toInt() == id) {
+                        var definition = type as NPCType?
+                        if (type.getTransforms() != null) {
+                            definition = transform(type)
+                        }
+                        return definition
+                    }
+                    type = type.getNext()
+                }
+            }
+        }
+        return null
+    }
+
+    private fun transform(type: NPCType): NPCType? {
+        var var1 = -1
+        val transforms = type.getTransforms()
+
+        if (type.getTransformVarbit() != -1) {
+            var1 = this.ctx?.client?.getVarbit(type.getTransformVarbit())!!
+        } else if (type.getTransformVarp() != -1) {
+            var1 = this.ctx?.client?.getVarps_main()!![type.getTransformVarp()]
+        }
+
+        val var2 = if (var1 >= 0 && var1 < transforms.lastIndex) {
+            transforms[var1]
+        } else {
+            transforms[transforms.lastIndex]
+        }
+
+        return if (var2 == -1) null else getType(var2)
     }
 }
